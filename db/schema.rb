d39095cb88dff5393854ef333b031f39aa5f77de
Ricky Brown <ricky@brilliantdeveloping.com>
2015-07-14 09:45:36 -0700

    installed devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714001957) do
+ActiveRecord::Schema.define(version: 20150714003205) do
 
   create_table "tickets", force: :cascade do |t|
     t.string   "subject"
@@ -28,5 +28,23 @@ ActiveRecord::Schema.define(version: 20150714001957) do
   add_index "tickets", ["priority"], name: "index_tickets_on_priority"
   add_index "tickets", ["status"], name: "index_tickets_on_status"
   add_index "tickets", ["user_id"], name: "index_tickets_on_user_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "login",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["login"], name: "index_users_on_login", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end

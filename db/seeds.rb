bbb066dbc7319cbe5ccc1de109e21686251d3681
Ricky Brown <ricky@brilliantdeveloping.com>
2015-07-15 11:05:26 -0700

    dummy seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+## GENERATE DUMMY TICKETS
+
+15.times do
+  Ticket.create(
+    subject: FFaker::Lorem.sentence,
+    content: FFaker::Lorem.paragraph,
+    status: 1,
+    priority: 2,
+    user_id: 1,
+    assignee_id: 2
+  )
+end
+
+puts 'Seeded Dummy Tickets'

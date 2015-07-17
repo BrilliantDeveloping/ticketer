62ba1ad3be2898360b7d4bf2ba1f731132afcc9d
Ricky Brown <ricky@brilliantdeveloping.com>
2015-07-17 09:31:00 -0700

    better seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,38 @@
-## GENERATE DUMMY TICKETS
+## GENERATE DUMMY DATA
 
 15.times do
   Ticket.create(
     subject: FFaker::Lorem.sentence,
     content: FFaker::Lorem.paragraph,
-    status: 1,
-    priority: 2,
-    user_id: 1,
-    assignee_id: 2
+    status: rand(1..6),
+    priority: rand(1..4),
+    user_id: rand(1..3),
+    assignee_id: rand(1..3)
   )
 end
 
-puts 'Seeded Dummy Tickets'
+User.find_or_create_by(
+  email: 'user1@test.com'
+  ) do |u|
+    u.username = 'testuser1'
+    u.password = 'password'
+    u.password_confirmation = 'password'
+end
+
+User.find_or_create_by(
+  email: 'user2@test.com'
+  ) do |u|
+    u.username = 'testuser2'
+    u.password = 'password'
+    u.password_confirmation = 'password'
+end
+
+User.find_or_create_by(
+  email: 'user3@test.com'
+  ) do |u|
+    u.username = 'testuser3'
+    u.password = 'password'
+    u.password_confirmation = 'password'
+end
+
+puts 'Seeded Database'
